629bd547fc158207d62f3d006b9e1c7a470dae7e
Avan <60692794+Layouwen@users.noreply.github.com>
2022-01-19 16:21:52 +0800

    feat: add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +14,21 @@ async function bootstrap() {
   // app.enable();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('蓝鲸记账api文档')
+    .setDescription('技术团队：梁又文、梁金俊')
+    .setVersion('1.0')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    ignoreGlobalPrefix: false,
+  };
+
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: '蓝鲸记账api文档',
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('doc', app, document, customOptions);
   await app.listen(3000);
 }
 
