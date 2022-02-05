eeb17bbf7a1981ec6e938ff38c1a11650c0a29ea
Avan <layouwen@gmail.com>
2022-02-05 14:51:16 +0800

    fix: improve token verification

--- src/middleware/UserInitMiddleware.ts
+++ src/middleware/UserInitMiddleware.ts
@@ -2,22 +2,19 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import { IRequest } from '../../custom';
+import config from '../config';
 
 @Injectable()
 export class UserInitMiddleware implements NestMiddleware {
   use(req: IRequest, res: Response, next: NextFunction) {
-    const token = req.headers.authorization;
-    if (token) {
-      const { payload } = jwt.decode(token.replace('Bearer ', ''), {
-        complete: true,
-      });
-      if (payload.id) {
-        req.info = {
-          id: payload.id,
-          username: payload.username,
-        };
-      }
-    }
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    try {
+      const decoded = jwt.verify(token, config.secret) as {
+        id: number;
+        username: string;
+      };
+      req.info = { id: decoded.id, username: decoded.username };
+    } catch (e) {}
     next();
   }
 }
