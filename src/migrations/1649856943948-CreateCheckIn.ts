46cf39819ea62c09265ca03dbfa0ebe1c7ffd173
Avan <layouwen@gmail.com>
2022-04-13 21:46:05 +0800

    chore: rename checkIn to CreateCheckIn

--- src/migrations/1649856943948-CreateCheckIn.ts
+++ src/migrations/1649856943948-CreateCheckIn.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class checkIn1649856647682 implements MigrationInterface {
+export class CreateCheckIn1649856943948 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
