f1b8c9e76270e19b70776d82d3d0821376939624
Avan <layouwen@gmail.com>
2022-04-13 21:12:29 +0800

    feat: add create comment migration

---
+++ src/migrations/1649854448624-createComment.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createComment1649854448624 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comment',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+            onUpdate: 'CURRENT_TIMESTAMP',
+          },
+          {
+            name: 'userId',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'topicId',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'replyToId',
+            type: 'int',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('comment');
+  }
+}
